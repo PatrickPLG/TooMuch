f6194b54465088ad83d72ea9402c10e9f6a2404c
Patrick <p-l-g@hotmail.dk>
2021-04-26 14:44:18 +0200

    ø

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, redirect, session, url_for, escape, g
+import sqlite3
+import os
+
+app = Flask(__name__)
+app.secret_key = 'BAD_SECRET_KEY'
+
+@app.route('/')
+def index():
+    with sqlite3.connect("db.db") as db:
+        try:
+            pass
+        except sqlite3.Error:
+            message = "There was a problem executing the SQL statement"
+            return render_template("index.html")
+    return render_template('index.html')
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    with sqlite3.connect("db.db") as db:
+        try:
+            if request.method == 'POST':
+                beskrivelse = request.form.get('beskrivelse')
+                udløbsdato = request.form.get('udløbsdato')
+                lokation = request.form.get('lokation')
+
+                file = request.files["file"]
+                file.save(os.path.join("uploads", file.filename))
+
+                cursor = db.cursor()
+                cursor.execute("INSERT INTO Varer (beskrivelse, udløbsdato, lokation, datastore) VALUES (?, ?, ?, ?)", (beskrivelse, udløbsdato, lokation, file.filename))
+
+
+        except sqlite3.Error:
+            message = "There was a problem executing the SQL statement"
+            return render_template("create.html")
+    return render_template("create.html")
+app.run(host='0.0.0.0', port=81, debug=True)
